9da64e7920a0c8124cb6ed29e2ca35b6e948759c
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-25 16:27:23 +0100

    Return a string a nb of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, nb) {
+    if (nb < 0)
+        return "ERROR";
+    else {
+        let returnString = "";
+        for (let i = 0; i < nb; i++)
+            returnString += string;
+        return returnString;
+    }
 };
 
 // Do not edit below this line
