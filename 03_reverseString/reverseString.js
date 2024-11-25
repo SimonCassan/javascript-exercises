c33869a931e417ca66771fe91e929f408a7e3da7
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-25 16:47:26 +0100

    Solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,9 @@
 const reverseString = function (string) {
     const sizeOfString = string.length;
     let newString = "";
-    for (let i = sizeOfString; i >= 0; i--)
+    for (let i = sizeOfString - 1; i >= 0; i--)
         newString += string[i];
+    return newString;
 };
 
 // Do not edit below this line
