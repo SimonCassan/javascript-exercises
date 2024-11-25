1844dcb38dbe4c9b48f038ef584001877cc228af
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-25 16:37:27 +0100

    Initial commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const sizeOfString = string.length;
+    let newString = "";
+    for (let i = sizeOfString; i >= 0; i--)
+        newString += string[i];
 };
 
 // Do not edit below this line
