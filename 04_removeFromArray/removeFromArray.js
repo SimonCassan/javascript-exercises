d180642b4524f2b38012b0e895dd2fc36c4b5091
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-26 09:53:43 +0100

    Return value is useful

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,11 +2,12 @@ const removeFromArray = function (array, nb) {
     let newArray = [];
     let j = 0;
     for (let i = 0; i < array.length; i++) {
-        if (array[i] != nb) {
+        if (array[i] !== nb) {
             newArray[j] = array[i];
             j++;
         }
     }
+    return newArray;
 };
 
 // Do not edit below this line
