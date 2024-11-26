ebc4aea15569f20e5e244ff64308d3fe406afa7f
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-26 10:45:49 +0100

    Takes two nbs instead, check for nb in wrong order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,11 @@
-const sumAll = function (...args) {
-    let sum = 0;
-    for (let i = 0; i < args.length; i++) {
-        if (typeof (args[i]) == number) {
-            sum += args[i];
-        }
+const sumAll = function (nb1, nb2) {
+    if (nb1 > nb2)
+        [nb1, nb2] = [nb2, nb1];
+    let sum = nb1;
+    let currentNb = ++nb1;
+    while (currentNb <= nb2) {
+        sum += currentNb;
+        currentNb++;
     }
     return sum;
 };
