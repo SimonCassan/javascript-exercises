0706ae4fef220953254c34764375efbd51fca489
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-26 10:27:26 +0100

    Initial commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function (...args) {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        if (typeof (args[i]) == number) {
+            sum += args[i];
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
