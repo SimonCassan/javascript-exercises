518e792ea3aae351c71235ec03fbed053fc34c20
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-26 09:48:29 +0100

    Initial try

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, nb) {
+    let newArray = [];
+    let j = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] != nb) {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
 };
 
 // Do not edit below this line
