a2288987ef57e3f2150a4773220f35a603e7bae7
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-27 14:21:17 +0100

    Solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,18 @@
 const sumAll = function (nb1, nb2) {
-    if (nb1 > nb2)
-        [nb1, nb2] = [nb2, nb1];
-    let sum = nb1;
-    let currentNb = ++nb1;
-    while (currentNb <= nb2) {
-        sum += currentNb;
-        currentNb++;
+    if (Number.isInteger(nb1) && Number.isInteger(nb2)) {
+        if (nb1 >= 0 && nb2 >= 0) {
+            if (nb1 > nb2)
+                [nb1, nb2] = [nb2, nb1];
+            let sum = nb1;
+            let currentNb = nb1 + 1;
+            while (currentNb <= nb2) {
+                sum += currentNb;
+                currentNb++;
+            }
+            return sum;
+        }
     }
-    return sum;
+    return "ERROR";
 };
 
 // Do not edit below this line
