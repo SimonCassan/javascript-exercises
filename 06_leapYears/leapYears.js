47b0bde215fc5c11cc8029efbff8b8bfa404c1a0
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2024-11-27 15:35:51 +0100

    Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (Number.isInteger(year)) {
+        // if year divisible by 0 OR (year not divisible by 100 AND year 
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0))
+            return true;
+        return false;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
